affac83f413d31bf3942c9affc75211e7875b6f2
adria66 <a.salvadorcazorla@gmail.com>
2020-12-11 16:57:51 +0100

    gamesId styles

--- src/components/GamesId.js
+++ src/components/GamesId.js
@@ -3,6 +3,10 @@ import React from 'react'
 import '../styles/GamesId.css'
 
 import { Spinner } from 'reactstrap';
+import { Button } from 'reactstrap';
+import { Badge } from 'reactstrap';
+
+
 
 class GamesId extends React.Component {
 
@@ -32,18 +36,27 @@ class GamesId extends React.Component {
     }
 
     minimumRequirements = ()=>{
-        
+
             return(
-                <div>
-                    <p>{this.state.gamesInfo.minimum_system_requirements.graphics}</p>
-                    <p>{this.state.gamesInfo.minimum_system_requirements.memory}</p>
-                    <p>{this.state.gamesInfo.minimum_system_requirements.os}</p>
-                    <p>{this.state.gamesInfo.minimum_system_requirements.processor}</p>
-                    <p>{this.state.gamesInfo.minimum_system_requirements.storage}</p>
+                <div className="row">
+                    <div className="col-sm-6" id="column-requirement">
+                        <h5>Graphics</h5>
+                        <p>{this.state.gamesInfo.minimum_system_requirements.graphics}</p>
+                        <h5>Memory</h5>
+                        <p>{this.state.gamesInfo.minimum_system_requirements.memory}</p>
+                        <h5>OS</h5>
+                        <p>{this.state.gamesInfo.minimum_system_requirements.os}</p>
+                    </div>
+                    <div className="col-sm-6" id="column-requirement">
+                        <h5>Processor</h5>
+                        <p>{this.state.gamesInfo.minimum_system_requirements.processor}</p>
+                        <h5>Storage</h5>
+                        <p>{this.state.gamesInfo.minimum_system_requirements.storage}</p>
+                    </div>
                 </div>
-                
+
             )
-        
+
     }
 
     renderLoadingImage = ()=>{
@@ -51,31 +64,59 @@ class GamesId extends React.Component {
             <div className="spinner">
                 <Spinner color="secondary" />
             </div>
-        ) 
+        )
     }
-    
-    
+
+    buttonClick = () => {
+        const url = this.state.gamesInfo.game_url
+        window.open(url)
+    }
 
     render(){
-        // console.log(this.state.gamesInfo.minimum_system_requirements)
-        return(
-            <div className="all-content">
-                
-                <img src={this.state.gamesInfo.thumbnail} alt={this.state.gamesInfo.title} />
-                <h2>{this.state.gamesInfo.title}</h2>
-                {this.state.gamesInfo.genre}
-                <br/>
-                {this.state.gamesInfo.platform}
-                <br/>
-                {this.state.gamesInfo.description}
-                <br/>
-                {this.state.gamesInfo.developer}
-                <br/>
-                {this.state.gamesInfo.game_url}
-                <br/>
-                {this.state.gamesInfo.length === 0 ? this.renderLoadingImage() : this.minimumRequirements()}
-                {this.state.gamesInfo.length === 0 ? this.renderLoadingImage() : this.stateImages()}
 
+        return(
+
+            <div className="all-content">
+                <div className="individual-game-container">
+
+                    <div className="title-image">
+                        <h2>{this.state.gamesInfo.title}</h2>
+                        <img src={this.state.gamesInfo.thumbnail} alt={this.state.gamesInfo.title} />
+                        
+                        <div className="genre-platform">
+                            <Badge id="genre">{this.state.gamesInfo.genre}</Badge>
+                            <Badge id="genre">{this.state.gamesInfo.platform}</Badge>
+                        </div>
+
+                        <Button id="playGame-button" color="dark" onClick={this.buttonClick}>Play Game</Button>
+
+                    </div>
+
+
+                    <div className="info-game">
+
+                        <div className="description">
+                            <h2>Description</h2>
+                            <hr/>
+                            {this.state.gamesInfo.description}
+                            <hr/>
+                        </div>
+
+                        <div className="requirements">
+                            <h2>Minimum System Requirements</h2>
+                            <hr/>
+                            {this.state.gamesInfo.length === 0 ? this.renderLoadingImage() : this.minimumRequirements()}
+                            <hr/>
+                        </div>
+
+                        <div className="screenshots">
+                            <h2>{this.state.gamesInfo.title} Screenshots</h2>
+                            <hr/>
+                            {this.state.gamesInfo.length === 0 ? this.renderLoadingImage() : this.stateImages()}
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
